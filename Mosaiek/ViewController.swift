3fe054ff31440a17a0a4e6ff0b4e1f46790a9b08
Jonathan Schapiro <jschapiro@fishsixrc.com>
2016-01-19 14:40:32 -0800

    Initial Commit

---
+++ Mosaiek/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Mosaiek
+//
+//  Created by Jonathan Schapiro on 1/19/16.
+//  Copyright © 2016 Jonathan Schapiro. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
