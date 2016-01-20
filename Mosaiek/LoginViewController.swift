07cca821ad68aa85eb12eff8e40707d5ff1959f5
Jonathan Schapiro <jschapiro@fishsixrc.com>
2016-01-19 18:47:38 -0800

    login functionality added

--- Mosaiek/LoginViewController.swift
+++ Mosaiek/LoginViewController.swift
@@ -9,12 +9,11 @@
 import UIKit
 import ParseFacebookUtilsV4
 
-class ViewController: UIViewController {
+class LoginViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
-        self.loginWithFacebook()
     }
 
     override func didReceiveMemoryWarning() {
@@ -23,6 +22,7 @@ class ViewController: UIViewController {
     }
     
     func loginWithFacebook() {
+        
         let permissions = ["email","public_profile","user_friends","user_photos"]
         PFFacebookUtils.logInInBackgroundWithReadPermissions(permissions) {
             (user: PFUser?, error: NSError?) -> Void in
@@ -36,8 +36,20 @@ class ViewController: UIViewController {
                 print("Uh oh. The user cancelled the Facebook login.")
             }
         }
+       
     }
+    
+    // Login Actions
 
+   
+    @IBAction func userLogin(sender: AnyObject) {
+        if (PFUser.currentUser() == nil){
+            self.loginWithFacebook()
+        } else {
+            //perform segue
+            print("User Already Logged In:", PFUser.currentUser()?.username)
+        }
+    }
 
 }
 
