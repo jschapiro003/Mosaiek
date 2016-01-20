3d7609585cf2ae37a4b9f2d65657f4669b0b6d4b
Jonathan Schapiro <jschapiro@fishsixrc.com>
2016-01-20 15:39:36 -0800

    Image Picker Functioning

--- Mosaiek/NewMosaicViewController.swift
+++ Mosaiek/NewMosaicViewController.swift
@@ -7,14 +7,24 @@
 //
 
 import UIKit
+import MobileCoreServices
 
-class NewMosaicViewController: UIViewController {
+class NewMosaicViewController: UIViewController, UINavigationControllerDelegate, UIImagePickerControllerDelegate {
+    
+    @IBOutlet weak var mosaicName: UITextField!
 
+    @IBOutlet weak var mosaicDescription: UITextField!
+    
+    @IBOutlet weak var mosaicImage: UIImageView!
+    
+    var imagePicker: UIImagePickerController!
+    
     override func viewDidLoad() {
-        super.viewDidLoad()
         print("Welcome to the new mosaic view controller")
-
-        // Do any additional setup after loading the view.
+        super.viewDidLoad()
+        
+        self.viewSetup()
+        
     }
 
     override func didReceiveMemoryWarning() {
@@ -22,6 +32,88 @@ class NewMosaicViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     
+    func viewSetup(){
+        
+        //add gesture recognizer to mosaicImageView
+        let mosaicImageViewTap = UITapGestureRecognizer()
+        mosaicImageViewTap.addTarget(self, action: "loadMosaicImage")
+        mosaicImage.addGestureRecognizer(mosaicImageViewTap)
+        mosaicImage.userInteractionEnabled = true
+        
+    }
+    
+    func loadMosaicImage(){
+        //lazy instantiation of imagePicker
+        if UIImagePickerController.isSourceTypeAvailable(UIImagePickerControllerSourceType.Camera){
+            
+            self.imagePicker = UIImagePickerController()
+            self.imagePicker.delegate = self
+            self.imagePicker.sourceType = UIImagePickerControllerSourceType.Camera;
+            self.imagePicker.mediaTypes = [kUTTypeImage as String]
+            self.imagePicker.allowsEditing = false
+            
+            self.presentViewController(self.imagePicker, animated: true, completion: nil)
+            
+        } else if UIImagePickerController.isSourceTypeAvailable(UIImagePickerControllerSourceType.PhotoLibrary){
+            
+            print("we going to the photo library")
+            self.imagePicker = UIImagePickerController()
+            self.imagePicker.delegate = self
+            self.imagePicker.sourceType = UIImagePickerControllerSourceType.PhotoLibrary;
+            self.imagePicker.mediaTypes = [kUTTypeImage as String]
+            self.imagePicker.allowsEditing = false
+            
+            self.presentViewController(self.imagePicker, animated: true, completion: nil)
+            
+        } else {
+            
+            print("You can't take no pics"); //generate uialertview
+        }
+        
+    }
+    
+    // Submit Form Actions
+    
+    @IBAction func inviteContributors(sender: AnyObject) {
+        
+        let formName = self.mosaicName.text!;
+        let formDescription = self.mosaicDescription.text!;
+        var formImageThumbnail:UIImage?
+        
+        if let formImage = self.mosaicImage.image {
+             formImageThumbnail = self.generateThumbnail(formImage);
+        }
+        
+        
+        print(formName,formDescription,formImageThumbnail)
+        self.performSegueWithIdentifier("showAddContributorsViewController", sender: self)
+    }
+    
+    func generateThumbnail(image:UIImage) -> UIImage{
+        
+        return image;
+    }
+    
+    //ImagePickerDelegate Methods
+    
+    func imagePickerController(picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : AnyObject]) {
+        
+        if let pickedImage = info[UIImagePickerControllerOriginalImage] as? UIImage {
+            print("image picker finished",pickedImage)
+            self.mosaicImage.image = pickedImage
+        }
+        
+        dismissViewControllerAnimated(true, completion: nil)
+    }
+    
+    
+    func imagePickerControllerDidCancel(picker: UIImagePickerController) {
+        print("image picker cancelled")
+        dismissViewControllerAnimated(true, completion: nil)
+    }
+    
+    
+   
 
     /*
     // MARK: - Navigation
